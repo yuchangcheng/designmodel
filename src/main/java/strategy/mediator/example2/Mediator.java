acbcd7df01c81a6920507e0899bfec3b3959192c
tom_glb <yuchangcheng@sunlands.com>
2018-11-20 17:11:50 +0800

    加注释

--- src/main/java/strategy/mediator/example2/Mediator.java
+++ src/main/java/strategy/mediator/example2/Mediator.java
@@ -2,7 +2,9 @@ package strategy.mediator.example2;
 
 /**
  * @Author: yuchangcheng
- * @Description:
+ * @Description:接口的作用是：封装隔离的
+ *                封装：对象的具体实现--》中介者对象的具体实现
+ *                隔离：实现对象、使用者--》中介者的具体实现对象 和 使用中介者的客户端对象
  * @Date: Create in 19:33 2018/11/19
  * @Modified By:
  */
